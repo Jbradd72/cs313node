eb1d86b5b882a1b0e5878c12c767dc687b0487f0
Jbradd72 <bra16024@byui.edu>
2018-11-17 15:17:21 -0700

    HHHHHHH

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const PORT = 80;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
-    .use(express.static("files"))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/week9', function (req, response){
-        fs.readFile("./week9/index.js", function (err, data) {
-            if (err) throw err;
-             response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(data);  
-        response.end(); 
-        });
-})
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- week8/helloworld.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var path = process.argv[2];
-var file = fs.readFile(path, function(err, data){
-    var count = data.toString().split("\n").length - 1;
-
-console.log(count);
-});
